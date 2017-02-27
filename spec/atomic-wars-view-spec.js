348cf231fb0bf074e4f376cd46edcb0e6394f31a
James Turner <mail.jt@gmail.com>
2017-02-27 14:31:26 +0000

    initial~

---
+++ spec/atomic-wars-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomicWarsView from '../lib/atomic-wars-view';
+
+describe('AtomicWarsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
