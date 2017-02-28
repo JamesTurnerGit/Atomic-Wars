e4c517479b3d5c922449a2fb9a62562573a0fcec
James Turner <mail.jt@gmail.com>
2017-02-28 10:44:21 +0000

    first test written

---
+++ spec/atomic-wars-model-spec.js
@@ -0,0 +1,27 @@
+'use babel';
+
+import AtomicWarsModel from '../lib/atomic-wars-model';
+
+describe('AtomicWarsModel', () => {
+  let workspaceElement, activationPromise;
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('atomic-wars');
+  });
+
+  describe('new', () => {
+    it('creates some windows', () => {
+    
+      atom.commands.dispatch(workspaceElement, 'atomic-wars:new');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        panes = atom.workspace.getPaneItems()
+        expect(panes.length).toBe(3)
+      });
+    });
+  })
+});
--- spec/atomic-wars-view-spec.js
+++ spec/atomic-wars-view-spec.js
@@ -1,9 +1,9 @@
-'use babel';
-
-import AtomicWarsView from '../lib/atomic-wars-view';
-
-describe('AtomicWarsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
+// 'use babel';
+//
+// import AtomicWarsView from '../lib/atomic-wars-view';
+//
+// describe('AtomicWarsView', () => {
+//   it('has one valid test', () => {
+//     expect('life').toBe('easy');
+//   });
+// });
