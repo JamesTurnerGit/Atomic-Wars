43e6028bd181615c50fb5389196d90678b220ba6
James Turner <mail.jt@gmail.com>
2017-02-28 12:18:00 +0000

    third test started

--- spec/atomic-wars-model-spec.js
+++ spec/atomic-wars-model-spec.js
@@ -1,6 +1,4 @@
- 'use babel';
-
-// import AtomicWarsModel from '../lib/atomic-wars-model';
+'use babel';
 
 describe('AtomicWarsModel', () => {
   let workspaceElement, activationPromise;
@@ -24,7 +22,26 @@ describe('AtomicWarsModel', () => {
 
     it('names panes correctly', () => {
       runs(() => {
+        descriptionWindowTitle = atom.workspace.getPaneItems()[0].getTitle()
+        codeWindowTitle        = atom.workspace.getPaneItems()[1].getTitle()
+        testsWindowTitle       = atom.workspace.getPaneItems()[2].getTitle()
+        expect(descriptionWindowTitle).toBe("description")
+        expect(testsWindowTitle).toBe("tests")
+        expect(codeWindowTitle).toBe("code")
+      })
+    })
+    it('populates panes with data', () => {
+      runs(() => {
+        descriptionWindowText = atom.workspace.getPaneItems()[0].getText()
+        codeWindowText        = atom.workspace.getPaneItems()[1].getText()
+        testsWindowText       = atom.workspace.getPaneItems()[2].getText()
 
+        expect(descriptionWindowText).toContain("language name")
+        expect(descriptionWindowText).toContain("name of kata")
+        expect(descriptionWindowText).toContain("Authors name")
+        expect(descriptionWindowText).toContain("some descrition text")
+        expect(testsWindowText).toBe("this is the starter codeblock")
+        expect(codeWindowText).toBe("this is the starting testblocks")
       })
     })
   })
