b742bcdd4147a48c4f7893f9a7335f87f972161d
James Turner <mail.jt@gmail.com>
2017-03-01 09:20:17 +0000

    moved code, added more todo's

--- spec/atomic-wars-Codewars-api-spec.js
+++ spec/atomic-wars-Codewars-api-spec.js
@@ -7,12 +7,13 @@ describe('AtomicWars-codewarsAPI', () => {
   let CWapi
   beforeEach(() => {
     CWapi = new CodeWarsAPI
+    atom.config.set('atomic-wars.CodeWarsAPIkey','4okdeUCSuvfPs6W9vqr5')// TODO: clean this out
   })
 
   describe('new', () => {
     it('returns a promise that resolves into a kata json', () => {
+      // TODO consider ways to test this after stubbing it out
       var url = 'https://www.codewars.com/api/v1/code-challenges/javascript/train'
-      atom.config.set('atomic-wars.CodeWarsAPIkey','4okdeUCSuvfPs6W9vqr5')// TODO: clean this out
       var result = CWapi.getKata(url)
       waitsForPromise(() =>
         result.then((messages) => {
